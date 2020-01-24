5551dd12a1fad1459e8de43de60392cf0b0292f5
ACR0S <alexcastroros@hotmail.com>
2020-01-24 17:22:56 +0100

    remove unnecessary function from bubble sorting script and updated example

--- src/bubble_sorting.ts
+++ src/bubble_sorting.ts
@@ -7,20 +7,15 @@ function permuteValues(a: number, b: number): number[]{
     return orderedList
 };
 
-function pseudoOrderList(numberList: number[], i: number, j: number): number[] {
-    [numberList[i], numberList[j]] = permuteValues(numberList[i], numberList[j]);
-    return numberList
-};
-
 function bubbleSort(numberList: number[]): number[]{
     let orderedList: number[] = numberList;
     for (var i = 0; i < numberList.length; i++){
         for (var j = i; j < numberList.length; j++) {
-            orderedList = pseudoOrderList(orderedList, i, j);
+            [orderedList[i], orderedList[j]] = permuteValues(orderedList[i], orderedList[j])
         }
     }
     return orderedList
 };
 
-let sampleList: number[] = [3, 2, 4, 1];
+let sampleList: number[] = [3, 3, 2, 4, 1, 1000, -20.5];
 console.log(bubbleSort(sampleList))
